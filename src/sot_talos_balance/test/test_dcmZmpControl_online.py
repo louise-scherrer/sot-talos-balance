77eeb99b69f517e8c38d01ad2ae0600bffb8eae6
Louise Scherrer <lscherrer@akasan.laas.fr>
2020-04-29 15:17:31 +0200

    [test_dcmZmpControl_online.py] Code cleaned

--- src/sot_talos_balance/test/test_dcmZmpControl_online.py
+++ src/sot_talos_balance/test/test_dcmZmpControl_online.py
@@ -1,30 +1,11 @@
-'''Test CoM admittance control as described in paper, with a Reference Velocity provided'''
+'''Test for online generation of movements for Talos using M. Naveau's pattern generator. Velocity command via signal robot.pg.velocitydes.value = (Vx, Vy, Omega_z)'''
 
 ## File adapted from appli_dcmZmpControl_file.py and appli_dcmZmpControl_online_ISA.py written by Isabelle Maroger
-## ALL THINGS ADDED OR CHANGED ARE COMMENTED IN CAPITAL LETTERS
 
 from sys import argv
-
-#from sot_talos_balance.utils.run_test_utils import ask_for_confirmation, run_ft_calibration, run_test, runCommandClient
-## INSTEAD, ISA WROTE (CHECK DIFFERENCES)
 from sot_talos_balance.utils.run_test_utils import *
 from time import sleep
-## END DIFFERENCE
-
 from dynamic_graph import * # for entity graph display
-
-## NEXT FEW LINES IRRELEVENT AS WISH TO GO ONLINE --> COMMENTED
-#try:
-#    # Python 2
-#    input = raw_input  # noqa
-#except NameError:
-#    pass
-#
-#test_folder = argv[1] if len(argv) > 1 else 'TestKajita2003WalkingOnSpot64/DSP20SSP780'
-#print('Using folder ' + test_folder)
-#
-#runCommandClient('test_folder = "' + test_folder + '"')
-## END DIFFERENCE
 
 run_test('appli_dcmZmpControl_online.py')
 
@@ -43,9 +24,8 @@ runCommandClient('robot.dcm_control.Ki.value = Ki_dcm')
 c = ask_for_confirmation('Execute trajectory?')
 if c:
     print('Executing the trajectory')
-#    runCommandClient('robot.triggerTrajGen.sin.value = 1') # NAMED DIFFERENTLY IN ISA'S SCRIPT, SEE BELOW
     runCommandClient('robot.triggerPG.sin.value = 1')
-    writeGraph('/local/lscherrer/lscherrer/Scripts/Results/my_dyn_graph.dot')
+#    writeGraph('/local/lscherrer/lscherrer/Scripts/Results/my_dyn_graph.dot')
 
 else:
     print('Not executing the trajectory')
@@ -54,6 +34,5 @@ input("Wait before dumping the data")
 
 runCommandClient('dump_tracer(robot.tracer)')
 
-## ADDED ;)
 print('Bye!')
 
